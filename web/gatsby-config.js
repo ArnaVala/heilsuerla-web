4a59803e2610ef4fa33f786620853f903bfc8705
ArnaVala <arnavala@gmail.com>
2021-03-20 12:50:55 +0000

    why is gatsby build not working

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,7 +9,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   plugins: [
-    'gatsby-plugin-postcss',
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-react-helmet',
     {
