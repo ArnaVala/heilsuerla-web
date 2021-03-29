669595ddba4e331babd6e147664bc1cba8130565
ArnaVala <arnavala@gmail.com>
2021-03-29 15:10:08 +0000

    this

--- web/src/components/common/Forms/validationSchema.js
+++ web/src/components/common/Forms/validationSchema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 const validationSchema = Yup.object().shape({
   name: Yup.string().min(2, 'Of stutt!').max(50, 'Of langt!').required('Vinsamlegast fylltu út reitinn.'),
   email: Yup.string().email('Vinsamlegast fylltu út rétt netfang.').required('Netfang er krafist.'),
-  message: Yup.string().required('Vinsamlegast skrifaðu skilaboð.')
+  message: Yup.string().required('Hættu þessu.')
 })
 
 export default validationSchema
