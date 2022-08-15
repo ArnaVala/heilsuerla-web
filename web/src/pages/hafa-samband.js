d2bc9a176fb4c774cbe378b949bdbb093ac1a1fa
Arna Vala <arnavala@gmail.com>
2022-08-15 23:26:45 +0000

    Create hafa-samband.js

---
+++ web/src/pages/hafa-samband.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import GraphQLErrorList from '../components/atoms/graphql-error-list'
+import SEO from '../components/atoms/seo'
+import Layout from '../components/organisms/layout'
+import {Container} from 'theme-ui'
+import PageBuilder from '../components/organisms/pageBuilder'
+
+import HeilsuContact from '../components/common/Forms/heilsuvidtalContact'
+
+export const query = graphql`
+  query HafaSambandPageQuery {
+    vidtalPage: sanityPage(slug: {current: {eq: "boka-heilsuvidtal"}}) {
+      id
+      title
+      slug {
+        current
+      }
+      ...PageBuilder
+    }
+  }
+`
+
+const HafaSambandPage = (props) => {
+  const {data, errors} = props
+  const vidtalPage = data && data.vidtalPage
+  const {pageBuilder, _rawPageBuilder} = vidtalPage
+
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout>
+      <SEO title={vidtalPage.title} />
+      <h1 hidden>Velkomin á {vidtalPage.title}</h1>
+
+      <Container>
+        <PageBuilder pageBuilder={pageBuilder} _rawPageBuilder={_rawPageBuilder} />
+        <HeilsuContact />
+      </Container>
+    </Layout>
+  )
+}
+
+export default HafaSambandPage
